718b7f0fec989eb9f11ec85cf8d74c449d522767
Stefan Scherfke <stefan.scherfke@offis.de>
2014-05-05 13:24:09 +0200

    Bump version from 2.0a1 to 2.0a2

--- mosaik/__init__.py
+++ mosaik/__init__.py
@@ -8,5 +8,5 @@ Currently, this is only :class:`mosaik.scenario.World`.
 from mosaik.scenario import World
 
 
-__version__ = '2.0a1'
+__version__ = '2.0a2'
 __all__ = ['World']
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mosaik',
-    version='2.0a1',
+    version='2.0a2',
     author='Stefan Scherfke',
     author_email='stefan.scherfke at offis.de',
     description=('Mosaik is a flexible Smart-Grid co-simulation framework.'),
