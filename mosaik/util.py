d8ae2772a07430c40b4c69b92a6571ca977ef8ba
Stefan Scherfke <stefan.scherfke@offis.de>
2014-09-01 17:38:55 +0200

    Added "mosaik.util.connect_randomly()" that randomly connects one set of entities to another set.

--- mosaik/util.py
+++ mosaik/util.py
@@ -3,6 +3,7 @@ This module contains some utility functions and classes.
 
 """
 import collections
+import random
 import sys
 
 from simpy.io.network import RemoteException
@@ -59,3 +60,88 @@ def sync_process(generator, world, *, ignore_errors=False):
         print('Mosaik terminating')
         world.shutdown()
         sys.exit(1)
+
+
+def connect_randomly(world, src_set, dest_set, *attrs, evenly=True,
+                     max_connects=float('inf')):
+    """Randomly :meth:`~mosaik.scenario.World.connect` the entities from
+    *src_set* to the entities from *dest_set* and return a subset of *dest_set*
+    containing all entities with a connection.
+
+    *world* is an instance of the :class:`~mosaik.scenario.World` to which the
+    entities belong.
+
+    *src_set* and *dest_set* are iterables containing
+    :class:`~mosaik.scenario.Entity` instances. *src_set* may be empty,
+    *dest_set* must not be empty. Each entity of *src_set* will be connected to
+    an entity of *dest_set*, but not every entity of *dest_set* will
+    necessarily have a connection (e.g., if you connect a set of three entities
+    to a set of four entities). A set of all entities from *dest_set*, to which
+    at least one entity from *src_set* was connected, will be returned.
+
+    *attrs* is a list of attribute names of pairs as in
+    :meth:`~mosaik.scenario.World.connect()`.
+
+    If the flag *evenly* is set to ``True``, entities connections will be
+    distributed as evenly as possible. That means if you connect a set of three
+    entities to a set of three entities, there will be three 1:1 connections;
+    if you connect four entities to three entities, there will be one 2:1 and
+    two 1:1 connections. If *evenly* is set to ``False``, connections will be
+    truly random. That means if you connect three entities to three entities,
+    you may either have three 1:1 connections, one 2:1 and two 1:1 connections
+    or just one 3:1 connection.
+
+    *max_connects* lets you set the maximum number of connections that an
+    entity of *dest_set* may receive. This argument is only taken into account
+    if *evenly* is set to ``False``.
+
+    """
+    dest_set = list(dest_set)
+    assert dest_set
+
+    if evenly:
+        connected = _connect_evenly(world, src_set, dest_set, *attrs)
+    else:
+        connected = _connect_randomly(world, src_set, dest_set, *attrs,
+                                      max_connects=max_connects)
+
+    return connected
+
+
+def _connect_evenly(world, src_set, dest_set, *attrs):
+    connect = world.connect
+    connected = set()
+
+    src_size, dest_size = len(src_set), len(dest_set)
+    pos = 0
+    while pos < src_size:
+        random.shuffle(dest_set)
+        for src, dest in zip(src_set[pos:], dest_set):
+            connect(src, dest, *attrs)
+            connected.add(dest)
+        pos += dest_size
+
+    return connected
+
+
+def _connect_randomly(world, src_set, dest_set, *attrs,
+                      max_connects=float('inf')):
+    connect = world.connect
+    connected = set()
+
+    assert len(src_set) <= (len(dest_set) * max_connects)
+    max_i = len(dest_set) - 1
+    randint = random.randint
+    connects = {}
+    for src in src_set:
+        i = randint(0, max_i)
+        dest = dest_set[i]
+        connect(src, dest, *attrs)
+        connected.add(dest)
+        connects[dest] = connects.get(dest, 0) + 1
+        if connects[dest] >= max_connects:
+            dest_set.remove(dest)
+            max_i -= 1
+            assert max_i >= 0
+
+    return connected
--- tests/test_util.py
+++ tests/test_util.py
@@ -1,3 +1,6 @@
+import collections
+import random
+
 from simpy.io.network import RemoteException
 import pytest
 
@@ -58,3 +61,67 @@ def test_sync_process_ignore_errors():
         yield world.env.event()
 
     util.sync_process(gen(), world, ignore_errors=True)
+
+
+@pytest.mark.parametrize(['src_size', 'dest_size', 'evenly', 'max_c',
+                          'dest_connects'], [
+    (20, 20, True, None, (1, 1)),
+    (0,  20, True, None, (0, 0)),
+    (12, 20, True, None, (0, 1)),
+    (20, 12, True, None, (1, 2)),
+    (20,  1, True, None, (20, 20)),
+    (42, 20, True, None, (2, 3)),
+    (20, 20, False, float('inf'), (0, 20)),
+    (0,  20, False, float('inf'), (0, 0)),
+    (12, 20, False, float('inf'), (0, 12)),
+    (20, 12, False, float('inf'), (0, 20)),
+    (20,  1, False, float('inf'), (20, 20)),
+    (90, 20, False, float('inf'), (0, 90)),
+    (90, 20, False, 6, (0, 6)),
+])
+def test_connect_randomly(src_size, dest_size, evenly, max_c, dest_connects):
+    """Test if connect_randomly() connects the correct amount of entities.
+
+    *src_size* and *dest_size* denote the size of the src/dest entity sets.
+
+    *evenly* and *max_c* are passed as keyword arguments to the function.
+
+    *dest_connects* is a ``(min, max)`` tuple describing how many entities of
+    the dest set have at least or most to be connected.
+
+    """
+    class World:
+        def __init__(self):
+            self.src_connects = set()
+            self.dest_connects = collections.defaultdict(lambda: 0)
+
+        def connect(self, src, dest, *attr_pairs):
+            self.src_connects.add(src)
+            self.dest_connects[dest] += 1
+
+    for seed in range(100):
+        random.seed(seed)
+        world = World()
+        src_set = [object() for i in range(src_size)]
+        dest_set = [object() for i in range(dest_size)]
+
+        connected = util.connect_randomly(world, src_set, dest_set, 'a', 'b',
+                                          evenly=evenly, max_connects=max_c)
+        if evenly:
+            assert len(connected) == min(src_size, dest_size)
+        else:
+            assert 0 <= len(connected) <= min(src_size, dest_size)
+
+        assert len(world.src_connects) == src_size
+        assert len(world.dest_connects) == len(connected)
+        if src_size == 0:
+            assert not world.dest_connects
+        else:
+            assert min(world.dest_connects.values()) >= dest_connects[0]
+            assert max(world.dest_connects.values()) <= dest_connects[1]
+
+
+def test_connect_randomly_errors():
+    # dest_set is empty
+    # dest_set too small for src_set and max_connects
+    pass
