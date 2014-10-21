62d2589dbb12df9192063cb90b1b3a7d44a309b0
Stefan Scherfke <stefan.scherfke@offis.de>
2014-10-21 10:21:17 +0200

    Added tests for the tutorial code.

---
+++ tests/test_tutorial.py
@@ -0,0 +1,27 @@
+"""
+Executes all files in /docs/tutorial/code for which an *.out file exists and
+compares the output of the script to the contents of the *.out file.
+
+"""
+import glob
+import os
+import subprocess
+import sys
+
+import pytest
+
+
+CODE_DIR = os.path.join(os.path.dirname(__file__), '..',
+                        'docs', 'tutorial', 'code')
+CASES = glob.glob(os.path.join(CODE_DIR, '*.out'))
+
+
+@pytest.mark.parametrize('outfile', CASES)
+def test_tutorial(outfile, monkeypatch):
+    pyfile = outfile.rsplit('.', 1)[0] + '.py'
+    expected = open(outfile).read()
+    # monkeypatch.syspath_prepend(CODE_DIR)
+    # monkeypatch.chdir(CODE_DIR)
+    out = subprocess.check_output([sys.executable, pyfile], cwd=CODE_DIR,
+                                  universal_newlines=True)
+    assert out == expected
