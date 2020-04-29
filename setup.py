f347e521f957f49646258dd3dd39ea1c297fd901
Thomas Raub <thomas.raub@offis.de>
2020-04-29 18:20:53 +0200

    Constrain simpy to version <4 due to simpy.io incompatibility

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     install_requires=[
         'networkx>=2.0',
         'mosaik-api>=2.3',
-        'simpy>=3.0.10',
+        'simpy>=3.0.10,<4.0.0',
         'simpy.io>=0.2.3',
     ],
 
