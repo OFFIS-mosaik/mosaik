917db16d3f49560e6d61677aa20198825749d4c7
Thomas Raub <thomas.raub@offis.de>
2020-07-15 22:38:58 +0200

    Additional test scenario for step's return dictionary

--- tests/test_mosaik.py
+++ tests/test_mosaik.py
@@ -28,7 +28,7 @@ sim_config_test = {
 test_cases = [('scenario_%s' % (i + 1), sim_config_local) for i in range(6)]
 test_cases.append(('scenario_5', sim_config_remote))
 test_cases.append(('scenario_6', sim_config_remote))
-test_cases.extend([('scenario_%s' % (i + 1), sim_config_test) for i in range(6, 13)])
+test_cases.extend([('scenario_%s' % (i + 1), sim_config_test) for i in range(6, 14)])
 
 
 # Test all combinations of both sim configs and the 5 test scenarios.
