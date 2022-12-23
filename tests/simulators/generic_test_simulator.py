fed83261ccc5966afc84ea2acd268e734a9bade4
Tobias Brandt <t.brandt91@gmail.com>
2022-12-23 11:03:02 +0100

    #92 Add test case for edge case where mosaik forgets trigger connections and add trigger_in connection in generic test simulator.
    All kudos to Eike, I've only converted his example from the issue to a test case.

--- tests/simulators/generic_test_simulator.py
+++ tests/simulators/generic_test_simulator.py
@@ -30,15 +30,16 @@ import copy
 from time import sleep
 
 
-logger = logging.getLogger('test_simulator')
+logger = logging.getLogger("test_simulator")
 
 
 sim_meta = {
-    'models': {
-        'A': {
-            'public': True,
-            'params': [],
-            'attrs': ['val_in', 'val_out'],
+    "models": {
+        "A": {
+            "public": True,
+            "params": [],
+            "attrs": ["val_in", "val_out", "trigger_in"],
+            "trigger": ["trigger_in"],
         },
     },
 }
@@ -53,29 +54,37 @@ class TestSim(mosaik_api.Simulator):
         self.value = None
         self.event_setter_wait = None
 
-    def init(self, sid, time_resolution, step_type='time-based', step_size=1, trigger=None,
-             self_steps={}, wallclock_duration=0., output_timing=None,
-             events={}):
+    def init(
+        self,
+        sid,
+        time_resolution,
+        step_type="time-based",
+        step_size=1,
+        trigger=None,
+        self_steps={},
+        wallclock_duration=0.0,
+        output_timing=None,
+        events={},
+    ):
         self.sid = sid
         self.step_type = step_type
-        self.meta['type'] = step_type
+        self.meta["type"] = step_type
         self.step_size = step_size
         self.self_steps = {float(key): val for key, val in self_steps.items()}
         self.wallclock_duration = wallclock_duration
         if output_timing:
-            output_timing = {float(key): val
-                             for key, val in output_timing.items()}
+            output_timing = {float(key): val for key, val in output_timing.items()}
         self.output_timing = output_timing
 
         if trigger:
-            self.meta['models']['A']['trigger'] = trigger
+            self.meta["models"]["A"]["trigger"] = trigger
 
-        if step_type == 'hybrid':
-            self.meta['models']['A']['persistent'] = ['val_out']
+        if step_type == "hybrid":
+            self.meta["models"]["A"]["persistent"] = ["val_out"]
 
         self.events = {float(key): val for key, val in events.items()}
         if events:
-            self.meta['set_events'] = True
+            self.meta["set_events"] = True
 
         return self.meta
 
@@ -83,14 +92,14 @@ class TestSim(mosaik_api.Simulator):
         n_entities = len(self.entities)
         new_entities = [str(eid) for eid in range(n_entities, n_entities + num)]
         self.entities.extend(new_entities)
-        return [{'eid': eid, 'type': model} for eid in new_entities]
+        return [{"eid": eid, "type": model} for eid in new_entities]
 
     def step(self, time, inputs, max_advance):
         self.time = time
         if self.wallclock_duration:
             sleep(self.wallclock_duration)
 
-        if self.step_type == 'time-based':
+        if self.step_type == "time-based":
             return time + self.step_size
         else:
             if time in self.self_steps:
@@ -100,7 +109,7 @@ class TestSim(mosaik_api.Simulator):
 
     def get_data(self, outputs):
         if self.output_timing is None:
-            data = {eid: {'val_out': self.time} for eid in self.entities}
+            data = {eid: {"val_out": self.time} for eid in self.entities}
         else:
             try:
                 current_output_timing = self.output_timing[self.time]
@@ -114,8 +123,10 @@ class TestSim(mosaik_api.Simulator):
             except:
                 output_time = None
             if output_time is not None:
-                data = {'time': output_time,
-                        **{eid: {'val_out': self.time} for eid in self.entities}}
+                data = {
+                    "time": output_time,
+                    **{eid: {"val_out": self.time} for eid in self.entities},
+                }
             else:
                 data = {}
         return data
@@ -135,5 +146,5 @@ class TestSim(mosaik_api.Simulator):
             last_time = real_time
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     mosaik_api.start_simulation(TestSim())
