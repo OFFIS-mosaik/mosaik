27e725d3eed8db6d9ee12f3081b37b6ebeccaa28
stomberg.malte <stomberg.malte@gmail.com>
2023-12-15 16:48:46 +0100

    data now only gets inserted into the connected output simulator entity

--- tests/test_basic_simulators.py
+++ tests/test_basic_simulators.py
@@ -1,18 +1,17 @@
-from pprint import pprint
 import mosaik
 import mosaik.util
 import mosaik.basic_simulators
 from mosaik.scenario import SimConfig
+from typing import Any, Dict, cast
+
 
 def test_basic_simulators():
     # Sim config. and other parameters
-    SIM_CONFIG:SimConfig  = {
-        'OutputSim': {
-            'python': 'mosaik.basic_simulators:OutputSimulator',
+    SIM_CONFIG: SimConfig = {
+        "OutputSim": {
+            "python": "mosaik.basic_simulators:OutputSimulator",
         },
-        'InputSim': {
-            'python': 'mosaik.basic_simulators:InputSimulator'
-        },
+        "InputSim": {"python": "mosaik.basic_simulators:InputSimulator"},
     }
 
     END = 15  # 15 seconds
@@ -25,19 +24,27 @@ def test_basic_simulators():
         return f"The time is {time}"
 
     # Start simulators
-    output_dict = world.start('OutputSim')
+    output_dict = world.start("OutputSim")
     output_model = output_dict.Dict.create(2)
 
-    input = world.start('InputSim', step_size=1)
+    input = world.start("InputSim", step_size=1)
     input_model_func = input.Function.create(1, function=sample_function)
     input_model_const = input.Constant.create(2, constant=2)
 
-
-    world.connect(input_model_func[0], output_model[1], 'value')
-    world.connect(input_model_const[0], output_model[0], 'value')
+    world.connect(input_model_func[0], output_model[1], "value")
+    world.connect(input_model_const[0], output_model[0], "value")
 
     # Run simulation
-    test_dict = output_dict.get_dict(output_model[1].eid)
+    test_dict: Dict[int, Dict[str, Any]] = cast(
+        Dict[int, Dict[str, Any]], output_dict.get_dict(output_model[1].eid)
+    )
+    test_input_eid = input_model_func[0].sid + "." + input_model_func[0].eid
+    print(test_input_eid)
     world.run(until=END)
-    assert(test_dict != {})
-    assert(len(test_dict) == END)
+
+    assert test_dict != {}
+    assert len(test_dict) == END
+    for key in test_dict:
+        print(test_dict[key])
+        assert test_dict[key]["value"][test_input_eid] == f"The time is {key}"
+test_basic_simulators()
