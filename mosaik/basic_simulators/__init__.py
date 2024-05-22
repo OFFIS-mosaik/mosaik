fd7e0b382e5012fb2492869f566b84bd2a096dbc
Eike <eike.schulte@offis.de>
2024-05-22 09:38:21 +0200

    Pipeline in rye, first step

--- mosaik/basic_simulators/__init__.py
+++ mosaik/basic_simulators/__init__.py
@@ -1,2 +1,2 @@
-from .input_simulator import InputSimulator
-from .output_simulator import OutputSimulator
+from .input_simulator import InputSimulator as InputSimulator
+from .output_simulator import OutputSimulator as OutputSimulator
