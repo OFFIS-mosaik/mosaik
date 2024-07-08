5de487a697b763de773b53c29b7ed983a4daaad8
Eike <eike.schulte@offis.de>
2024-07-08 15:30:18 +0200

    Make greeting fit in 80 column, add color

--- mosaik/greetings_util.py
+++ mosaik/greetings_util.py
@@ -6,21 +6,20 @@ import sys
 
 
 def print_greetings():
-    greetings = rf"""
-                     ____                              _ _
-                    /    \                            (_) |
-               ____/      \  _ __ ___   ___  ___  __ _ _| | __
-              /    \      / | '_ ` _ \ / _ \/ __|/ _` | | |/ / 
-             /      \____/  | | | | | | (_) \__ \ (_| | |   <     
-             \      /    \  |_| |_| |_|\___/|___/\__,_|_|_|\_\    
-              \____/      \____
-              /    \      /    \    mosaik version:     {mosaik_version}
-             /      \____/      \   mosaik API version: {api_version}
-             \      /    \      /   Python version:     {get_python_version()}
-              \____/      \____/    OS:                 {get_os()}
-                   \      /         Documentation:      https://mosaik.readthedocs.io/en/{mosaik_version}/
-                    \____/          Get in touch:       https://github.com/orgs/OFFIS-mosaik/discussions
-                    
+    greetings = f"""
+                \033[32m____\033[0m                              _ _
+               \033[32m/    \\\033[0m                            (_) |
+          \033[34m____\033[32m/      \\\033[0m  _ __ ___   ___  ___  __ _ _| | __
+         \033[34m/    \033[32m\\      /\033[0m | '_ ` _ \\ / _ \\/ __|/ _` | | |/ /
+        \033[34m/      \033[32m\\____/\033[0m  | | | | | | (_) \\__ \\ (_| | |   <
+        \033[34m\\      /\033[0m    \\  |_| |_| |_|\\___/|___/\\__,_|_|_|\\_\\
+         \033[34m\\____/\033[0m      \\\033[31m____\033[0m
+         \033[35m/    \\\033[0m      \033[31m/    \\\033[0m     mosaik: {mosaik_version}
+        \033[35m/      \\\033[33m____\033[31m/      \\\033[0m       API: {api_version}
+        \033[35m\\      /\033[33m    \\\033[31m      /\033[0m    Python: {get_python_version()}
+         \033[35m\\____/\033[33m      \\\033[31m____/\033[0m         OS: {get_os()}
+              \033[33m\\      /\033[0m            Docs: https://mosaik.readthedocs.io/en/{mosaik_version}/
+               \033[33m\\____/\033[0m     Get in touch: https://github.com/orgs/OFFIS-mosaik/discussions
         """
     print(dedent(greetings), file=sys.stderr)
 
