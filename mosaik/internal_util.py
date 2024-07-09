c0e5259f5390b348e56dad4e730682e4880f9843
Eike <eike.schulte@offis.de>
2024-07-09 09:18:04 +0200

    Fix version import

--- mosaik/internal_util.py
+++ mosaik/internal_util.py
@@ -1,7 +1,7 @@
 from typing import Callable, Dict
 from typing_extensions import TypeVar
 import platform
-from mosaik._version import __version__
+from mosaik._version import version
 
 K = TypeVar("K")
 V = TypeVar("V")
@@ -49,11 +49,8 @@ def merge_all(
 
 
 def doc_link(page: str, anchor: str) -> str:
-    return f"https://mosaik.readthedocs.io/en/{__version__}/{page}.html#{anchor}"
+    return f"https://mosaik.readthedocs.io/en/{version}/{page}.html#{anchor}"
 
-# Function to read the version from _version.py
-def read_version():
-    return __version__
 
 def get_python_version():
     return platform.python_version()
